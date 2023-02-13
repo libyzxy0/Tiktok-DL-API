b9be43c6735c5cc3f3dc58f55170967face730f6
Jan Liby Dela Costa <janlibydelacosta@gmail.com>
2023-02-13 08:44:52 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,25 +3,29 @@ const app = express()
 const request = require('request');
 
 
-app.get('/m', (req, res) => {
-  const getVideoID = (url, cb) => {
-request({ url: url, followRedirect: false }, function (err, res, body) {
-   let url = res.headers.location;
-   let data = url.split("/")
-    data.shift()
-    data.shift()
-    data.shift()
-    data.shift()
-    data.shift()
-    cb(data)
-});
-  }
-  getVideoID(req.query.url, (data) => {
-  const m = data[0];
-  var id = m.substring(0, 19);
-  let url = `https://www.tikwm.com/video/media/hdplay/${id}.mp4`
-  res.json({ link: url });
-})
+app.get('/', (req, res) => {
+  if(!req.query.url) {
+  	res.json({ msg: "Asan url mo bobo ka!" });
+  } else {
+  	const getVideoID = (url, cb) => {
+  	    request({ url: url, followRedirect: false }, function (err, res, body) {
+  	       let url = res.headers.location;
+             let data = url.split("/")
+             data.shift()
+             data.shift()
+             data.shift()
+             data.shift()
+             data.shift()
+             cb(data)
+           });
+        }
+      getVideoID(req.query.url, (data) => {
+      	const m = data[0];
+          var id = m.substring(0, 19);
+          let url = `https://www.tikwm.com/video/media/hdplay/${id}.mp4`;
+          res.json({ link: url });
+      })
+   }
 })
 
 app.listen(3000,() => console.log("Listening..."))
